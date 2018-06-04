b9e0606f3819e6e1ebe1600d1309afed3fed5b79
Eliran Maimon and Renee Sarley <renee@>
2018-06-04 09:42:51 -0700

    add new array with desired elements

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,17 @@ $(function(){
 
 
 $("form").submit(function(event){
-  var userFood = [$("input#food").val()];
+  var userFood = $("input#food").val();
+  var userColor = $("input#color").val();
+  var userAnimal = $("input#animal").val();
+  var userCity = $("input#city").val();
+  var userResponses =[userFood, userAnimal, userColor, userCity];
 
-  alert(userFood);
+  var newArray = [];
+
+  newArray.push(userResponses[1], userResponses[0], userResponses[2]);
+  alert(newArray);
+
 
   event.preventDefault();
 });
